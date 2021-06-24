cdb83825ec2f759ce861fe8cf24d3a64ce107c16
Calvin Wang <calvinwang@yahoo.com>
2021-06-23 21:03:17 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const playimg = document.getElementById('playicon');
+const playsound = document.getElementById('mySong');
+
+function playAudio() {
+   playsound.play();
+}
+
+function pauseAudio() {
+    playsound.pause();
+}
